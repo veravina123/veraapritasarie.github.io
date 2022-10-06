fa32448fc3c167916c14611c7c036bb673484e5e
veravina123 <113001325+veravina123@users.noreply.github.com>
2022-10-06 22:07:10 +0700

    Add files via upload

---
+++ p05-jquey-delete/script.js
@@ -0,0 +1,10 @@
+$(document).ready(() => {
+    $("td").click(function (event) {
+        alert("kolom berisi " + $(this).html() + " !");
+    });
+    $("th").click(function (event) {
+        alert("kolom berisi " + $(this).html() +" !");
+    });
+});
+Footer
+© 2022 GitHub, Inc.
